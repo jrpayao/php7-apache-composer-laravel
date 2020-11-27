de5420ca02964773d4ee442189fc9c7f8626a581
Claudio M Payao Junior <claudio.junior@engesoftware.com.br>
2020-11-27 00:10:17 -0300

    Subindo configuracoes Postgres

--- entrypoint/example.sh
+++ entrypoint/example.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
-
-if ! [ -f ".env" ]; then
-    cp .env.dist .env
-fi
+set -e
+echo "[ ****************** ] Back - Starting Endpoint of Application"
 
 if ! [ -d "./vendor" ]; then
+    echo "[ ****** ] Generating dependencies of Laravel with Artisan..."
+    echo "[ ****** ] Install dependencies whit composer..."
     composer install
     php artisan key:generate
 fi
 
+if ! [ -f ".env" ]; then
+    echo "[ ****** ] .env not found."
+    cp .env.dist .env
+
+    echo "[ ****** ] Key Generate"
+    php artisan key:generate
+
+    echo "[ ****** ] DB Migration"
+    php artisan migrate
+
+    echo "[ ****** ] DB Seed"
+    php artisan db:seed
+
+fi
+
 php artisan config:clear
 
+echo "[ ****** ] Create necessaries folders "
 mkdir -p ./storage/logs || true
 mkdir -p ./storage/framework || true
 mkdir -p ./storage/framework/cache || true
@@ -19,5 +35,14 @@ mkdir -p ./storage/framework/testing || true
 mkdir -p ./storage/framework/views || true
 
 
+if ! [ -d "./storage/fonts" ]; then
+    echo "[ ****** ] Create storage/fonts folder"
+    mkdir ./storage/fonts
+fi
+
+echo "[ ****** ] Giving write permission to storage folder"
+chmod -R 777 ./storage/*
+
+echo "[ ****************** ] Back - Ending Endpoint of Application"
 /usr/sbin/httpd -D FOREGROUND
 exec "$@"
